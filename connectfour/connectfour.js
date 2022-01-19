e5b97b94525206a99ed2ee44e22c93989f1287ac
Andrew <andrewmgunn26@gmail.com>
2022-01-19 14:05:15 -0600

    stoppping here

--- connectfour/connectfour.js
+++ connectfour/connectfour.js
@@ -1,12 +1,41 @@
-let board;
+let board = [];
+let player1 = 'X';
+let player2 = 'O';
+let currentPlayer = player1;
 
 function newGame() {
   createBoard();
   $('#winner').html('');
 }
 
-function createBoard() {
+function createBoard(board) {
+  board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+  ];
 
+  let html = '<table>';
+
+  for (let col in board) {
+    html += '<td><button class="drop"'
+  }
+
+  for (let row = 0; row < board.length; row++) {
+    html += '<tr>';
+    for (let col = 0; col < board[row].length; col++) {
+      html += '<td> </td>';
+
+    }
+    html += '</tr>';
+
+  }
+  html += '</table>';
+
+  $('#board').html(html);
 }
 
 function drop() {

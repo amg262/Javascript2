71ac3433dae15a6cdd8b65d21b79c7d248f5f910
Andrew <andrewmgunn26@gmail.com>
2022-01-19 13:37:28 -0600

    init

---
+++ connectfour/connectfour.js
@@ -0,0 +1,39 @@
+let board;
+
+function newGame() {
+  createBoard();
+  $('#winner').html('');
+}
+
+function createBoard() {
+
+}
+
+function drop() {
+
+}
+
+function putPiece() {
+
+}
+
+function check4win() {
+
+}
+
+function switchPlayer() {
+
+}
+
+function columnHover() {
+
+}
+
+function endGame() {
+
+}
+
+$(document).ready(function() {
+  $('#newGame').on('click', newGame);
+
+});
